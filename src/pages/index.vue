b88563d8baf7b1142eaa0ec2f21788c88cee24da
liumingxun <18945504808@163.com>
2022-01-24 11:45:59 +0800

    fixup! chore: update project

---
+++ src/pages/index.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>
+    <HelloWorld msg="123" />
+  </div>
+</template>
+
+<script lang='ts' setup>
+// import { reactive, ref } from 'vue'
+
+</script>
--- vite.config.ts
+++ vite.config.ts
@@ -4,28 +4,33 @@ import Vue from '@vitejs/plugin-vue'
 import Unocss from 'unocss/vite'
 import AutoImport from 'unplugin-auto-import/vite'
 import Components from 'unplugin-vue-components/vite'
+import Pages from 'vite-plugin-pages'
+import Layouts from 'vite-plugin-vue-layouts'
+import Markdown from 'vite-plugin-md'
 
 const pathResolve = (src: string) => resolve(__dirname, src)
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    Vue({ script: { refSugar: true }, include: [/\.vue$/] }),
+    Vue({ script: { refSugar: true }, include: [/\.vue$/, /\.md$/] }),
     Unocss(),
+    Pages({ extensions: ['vue', 'md'] }),
+    Layouts(),
     AutoImport({
       imports: ['vue', 'pinia', 'vue-router', '@vueuse/core'],
       dts: 'src/auto-imports.d.ts'
     }),
     Components({
-      extensions: ['vue'],
-      include: [/\.vue$/, /\.vue\?vue/],
+      extensions: ['vue', 'md'],
+      include: [/\.vue$/, /\.vue\?vue/, /\.md$/],
       dts: 'src/components.d.ts'
-    })
+    }),
+    Markdown()
   ],
   resolve: {
     alias: {
       '@': pathResolve('src'),
-      '@a': pathResolve('src/assets'),
-      '@v': pathResolve('src/views')
+      '@a': pathResolve('src/assets')
     }
   },
   optimizeDeps: {
